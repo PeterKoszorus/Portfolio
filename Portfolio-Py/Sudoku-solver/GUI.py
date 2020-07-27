fdde7a6b90c7c5bd081fd8335cbebd65de6c6f99
Peter <p.koszorus27@gmail.com>
2020-07-27 16:41:51 +0200

    Seted the background, title, logo and window size

--- Portfolio-Py/Sudoku-solver/GUI.py
+++ Portfolio-Py/Sudoku-solver/GUI.py
@@ -1,11 +1,21 @@
 import pygame
 
+WIDTH = 540
+HEIGHT = 600
+WHITE = [255, 255, 255]
+BLACK = [0, 0, 0]
+
 pygame.init()
 
-main_window = pygame.display.set_mode((400, 400))
-run = True
+main_window = pygame.display.set_mode((WIDTH, HEIGHT))
+main_window.fill(WHITE)
+pygame.display.set_caption("Sudoku")
+pygame.display.set_icon(pygame.image.load("Logo.png"))
 
+run = True
+#Main loop
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    pygame.display.update()
