9e3809677ac4363b484f4dab407bed6ebe429ff0
Peter <p.koszorus27@gmail.com>
2020-07-27 17:23:07 +0200

    Added main func

--- Portfolio-Py/Sudoku-solver/GUI.py
+++ Portfolio-Py/Sudoku-solver/GUI.py
@@ -1,21 +1,34 @@
 import pygame
 
+# screen settings
 WIDTH = 540
 HEIGHT = 600
+FPS = 30
+
+# colours
 WHITE = [255, 255, 255]
 BLACK = [0, 0, 0]
+GRAY = [200, 200, 200]
 
-pygame.init()
 
-main_window = pygame.display.set_mode((WIDTH, HEIGHT))
-main_window.fill(WHITE)
-pygame.display.set_caption("Sudoku")
-pygame.display.set_icon(pygame.image.load("Logo.png"))
+def main():
+    pygame.init()
 
-run = True
-#Main loop
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    pygame.display.update()
+    fps_clock = pygame.time.Clock()
+    main_window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    main_window.fill(WHITE)
+    pygame.display.set_caption("Sudoku")
+    pygame.display.set_icon(pygame.image.load("Logo.png"))
+
+    run = True
+    # Main loop
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        pygame.display.update()
+        fps_clock.tick(FPS)
+
+
+main()
